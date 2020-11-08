25bfec82c0ba151fc6481f4fce650e15a2c4903d
Igal <igalfsg@gmail.com>
2020-11-08 13:08:31 -0800

    added a bunch of resources

--- pulumi-test/MyStack.cs
+++ pulumi-test/MyStack.cs
@@ -6,12 +6,102 @@ using Pulumi.Azure.KeyVault;
 using Pulumi.Azure.KeyVault.Inputs;
 using Pulumi.Azure.AppService.Inputs;
 using Pulumi.Azure.AppInsights;
+using AzureAD = Pulumi.AzureAD;
+
+
+
 class MyStack : Stack
 {
     public MyStack()
     {
         // Create an Azure Resource Group
         var resourceGroup = new ResourceGroup("pulumiTestRG");
+
+        //create service bus
+        var serviceBus = new Pulumi.Azure.ServiceBus.Namespace("webappServiceBus", new Pulumi.Azure.ServiceBus.NamespaceArgs
+        {
+            Location = resourceGroup.Location,
+            ResourceGroupName = resourceGroup.Name,
+            Sku = "Standard",
+        });
+
+        //create ServiceBus Queue
+        var exampleQueue = new Pulumi.Azure.ServiceBus.Queue("exampleQueue", new Pulumi.Azure.ServiceBus.QueueArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            NamespaceName = serviceBus.Name,
+        });
+
+
+        //create sql 
+        var primary = new Pulumi.Azure.Sql.SqlServer("primary", new Pulumi.Azure.Sql.SqlServerArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Location = resourceGroup.Location,
+            Version = "12.0",
+            AdministratorLogin = "sqladmin",
+            AdministratorLoginPassword = "pa$$w0rd",
+        });
+        var secondary = new Pulumi.Azure.Sql.SqlServer("secondary", new Pulumi.Azure.Sql.SqlServerArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Location = "northeurope",
+            Version = "12.0",
+            AdministratorLogin = "sqladmin",
+            AdministratorLoginPassword = "pa$$w0rd",
+        });
+        
+        var sqlLogs = new Pulumi.Azure.Storage.Account("sqllogs", new Pulumi.Azure.Storage.AccountArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Location = resourceGroup.Location,
+            AccountTier = "Standard",
+            AccountReplicationType = "LRS",
+        });
+        var db1 = new Pulumi.Azure.Sql.Database("exampleDatabase", new Pulumi.Azure.Sql.DatabaseArgs
+        {
+            ResourceGroupName = primary.ResourceGroupName,
+            Location = primary.Location,
+            ServerName = primary.Name,
+            ExtendedAuditingPolicy = new Pulumi.Azure.Sql.Inputs.DatabaseExtendedAuditingPolicyArgs
+            {
+                StorageEndpoint = sqlLogs.PrimaryBlobEndpoint,
+                StorageAccountAccessKey = sqlLogs.PrimaryAccessKey,
+                StorageAccountAccessKeyIsSecondary = true,
+                RetentionInDays = 6,
+            },
+            RequestedServiceObjectiveName = "S0",
+            Tags = 
+            {
+                { "environment", "production" },
+            },
+        });
+        
+        
+
+        //create sql failover
+        var exampleFailoverGroup = new Pulumi.Azure.Sql.FailoverGroup("failover", new Pulumi.Azure.Sql.FailoverGroupArgs
+        {
+            ResourceGroupName = primary.ResourceGroupName,
+            ServerName = primary.Name,
+            Databases = 
+            {
+                db1.Id,
+            },
+            PartnerServers = 
+            {
+                new Pulumi.Azure.Sql.Inputs.FailoverGroupPartnerServerArgs
+                {
+                    Id = secondary.Id,
+                },
+            },
+            ReadWriteEndpointFailoverPolicy = new Pulumi.Azure.Sql.Inputs.FailoverGroupReadWriteEndpointFailoverPolicyArgs
+            {
+                Mode = "Automatic",
+                GraceMinutes = 60,
+            },
+        });
+
 
         // Create an Azure Storage Account
         // var storageAccount = new Account("pulumistoragetest", new AccountArgs
@@ -22,21 +112,21 @@ class MyStack : Stack
         // });
 
         var appServicePlan = new Plan("asp", new PlanArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Kind = "App",
+            Sku = new PlanSkuArgs
             {
-                ResourceGroupName = resourceGroup.Name,
-                Kind = "App",
-                Sku = new PlanSkuArgs
-                {
-                    Tier = "Basic",
-                    Size = "B1",
-                },
-            });
+                Tier = "Basic",
+                Size = "B1",
+            },
+        });
 
-        // var appInsights = new Insights("appInsights", new InsightsArgs
-        // {
-        //     ApplicationType = "web",
-        //     ResourceGroupName = resourceGroup.Name
-        // });
+        var appInsights = new Insights("appInsights", new InsightsArgs
+        {
+            ApplicationType = "web",
+            ResourceGroupName = resourceGroup.Name
+        });
 
         var webapp = new AppService("webapp", new AppServiceArgs
         {
@@ -45,7 +135,7 @@ class MyStack : Stack
             AppSettings =
             {
                 // {"WEBSITE_RUN_FROM_PACKAGE", codeBlobUrl},
-                // {"APPINSIGHTS_INSTRUMENTATIONKEY", appInsights.InstrumentationKey},
+                {"APPINSIGHTS_INSTRUMENTATIONKEY", appInsights.InstrumentationKey},
                 // {"APPLICATIONINSIGHTS_CONNECTION_STRING", appInsights.InstrumentationKey.Apply(key => $"InstrumentationKey={key}")},
                 {"ApplicationInsightsAgent_EXTENSION_VERSION", "~2"},
             },
@@ -55,9 +145,38 @@ class MyStack : Stack
                 Type = "SystemAssigned"
             },
         });
-        //ref https://www.pulumi.com/docs/reference/pkg/azure/keyvault/keyvault/
+        //get CurentUser
         var current = Output.Create(Pulumi.Azure.Core.GetClientConfig.InvokeAsync());
-        var exampleKeyVault = new KeyVault("exampleKeyVault", new Pulumi.Azure.KeyVault.KeyVaultArgs
+        
+        //create aad group
+        var appADGroup = new AzureAD.Group("mynewGroup", new AzureAD.GroupArgs
+        {
+            Owners = { current.Apply(current => current.ObjectId) },
+            Members = { webapp.Identity.Apply(x => x.PrincipalId) },
+        });
+
+        add aad admin to server
+        var primarySQLAADAdmin = new Pulumi.Azure.Sql.ActiveDirectoryAdministrator("exampleActiveDirectoryAdministrator", 
+            new Pulumi.Azure.Sql.ActiveDirectoryAdministratorArgs
+        {
+            ServerName = primary.Name,
+            Login = "sqladmin",
+            ResourceGroupName = primary.ResourceGroupName,
+            TenantId = current.Apply(current => current.TenantId),
+            ObjectId = appADGroup.ObjectId,
+        });
+        var secondarySQLAADAdmin = new Pulumi.Azure.Sql.ActiveDirectoryAdministrator("secondarysqlAdmin", 
+            new Pulumi.Azure.Sql.ActiveDirectoryAdministratorArgs
+        {
+            ServerName = secondary.Name,
+            Login = "sqladmin",
+            ResourceGroupName = secondary.ResourceGroupName,
+            TenantId = current.Apply(current => current.TenantId),
+            ObjectId = appADGroup.ObjectId,
+        });
+
+        //ref https://www.pulumi.com/docs/reference/pkg/azure/keyvault/keyvault/
+        var webappAKV = new KeyVault("webappAKV", new Pulumi.Azure.KeyVault.KeyVaultArgs
         {
             Location = resourceGroup.Location,
             ResourceGroupName = resourceGroup.Name,
@@ -67,21 +186,21 @@ class MyStack : Stack
             SoftDeleteRetentionDays = 7,
             PurgeProtectionEnabled = false,
             SkuName = "standard",
-            AccessPolicies = 
+            AccessPolicies =
             {
                 new KeyVaultAccessPolicyArgs
                 {
                     TenantId = current.Apply(current => current.TenantId),
                     ObjectId = current.Apply(current => current.ObjectId),
-                    KeyPermissions = 
+                    KeyPermissions =
                     {
                         "get",
                     },
-                    SecretPermissions = 
+                    SecretPermissions =
                     {
-                        "get",
+                        "set", "get", "list", "delete"
                     },
-                    StoragePermissions = 
+                    CertificatePermissions =
                     {
                         "get",
                     },
@@ -89,43 +208,43 @@ class MyStack : Stack
                 new KeyVaultAccessPolicyArgs
                 {
                     TenantId = current.Apply(current => current.TenantId),
-                    ObjectId =  webapp.Identity.Apply(x => x.PrincipalId),
-                    KeyPermissions = 
+                    ObjectId =  appADGroup.ObjectId,
+                    KeyPermissions =
                     {
                         "create",
                     },
-                    SecretPermissions = 
+                    SecretPermissions =
                     {
-                        "set",
+                        "set", "get", "list"
                     },
-                    StoragePermissions = 
+                    CertificatePermissions =
                     {
-                        "set",
+                        "get",
                     },
                 },
             },
-            NetworkAcls = new KeyVaultNetworkAclsArgs
-            {
-                DefaultAction = "Deny",
-                Bypass = "AzureServices",
-            },
-            Tags = 
+            Tags =
             {
                 { "environment", "Testing" },
             },
         });
 
+        var secret = new Secret("paymentApiKey", new SecretArgs
+        {
+            KeyVaultId = webappAKV.Id,
+            Value = "ddd"//serviceBus.Id,
+        });
 
         // Export the connection string for the storage account
-        // this.ConnectionString = storageAccount.PrimaryConnectionString;
-        this.appID = webapp.Identity.Apply(x => x.PrincipalId);
         this.Outbound = webapp.OutboundIpAddresses;
+        this.akvurl = webappAKV.VaultUri;
+        this.secretURL = secret.Id;
     }
 
     [Output]
-    // public Output<string> ConnectionString { get; set; }
-    public Output<string> appID { get; set; }
-    [Output]
     public Output<string> Outbound { get; set; }
-
+    [Output]
+    public Output<string> akvurl { get; set; }
+    [Output]
+    public Output<string> secretURL { get; set; }
 }
