26f8800a55add39fd0bd0f16fc5cf07dbb7873f2
Igal <igalfsg@gmail.com>
2020-11-07 19:49:12 -0800

    can compile now

--- pulumi-test/MyStack.cs
+++ pulumi-test/MyStack.cs
@@ -1,16 +1,20 @@
 using Pulumi;
 using Pulumi.Azure.Core;
 using Pulumi.Azure.Storage;
-
+using Pulumi.Azure.AppService;
+using Pulumi.Azure.KeyVault;
+using Pulumi.Azure.KeyVault.Inputs;
+using Pulumi.Azure.AppService.Inputs;
+using Pulumi.Azure.AppInsights;
 class MyStack : Stack
 {
     public MyStack()
     {
         // Create an Azure Resource Group
-        var resourceGroup = new ResourceGroup("resourceGroup");
+        var resourceGroup = new ResourceGroup("pulumiTestRG");
 
         // Create an Azure Storage Account
-        var storageAccount = new Account("storage", new AccountArgs
+        var storageAccount = new Account("pulumistoragetest", new AccountArgs
         {
             ResourceGroupName = resourceGroup.Name,
             AccountReplicationType = "LRS",
@@ -28,11 +32,35 @@ class MyStack : Stack
                 },
             });
 
-        //ref https://www.pulumi.com/docs/reference/pkg/azure/keyvault/keyvault/
-        var exampleKeyVault = new Azure.KeyVault.KeyVault("exampleKeyVault", new Azure.KeyVault.KeyVaultArgs
+        var appInsights = new Insights("appInsights", new InsightsArgs
         {
-            Location = exampleResourceGroup.Location,
-            ResourceGroupName = exampleResourceGroup.Name,
+            ApplicationType = "web",
+            ResourceGroupName = resourceGroup.Name
+        });
+
+        var webapp = new AppService("webapp", new AppServiceArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            AppServicePlanId = appServicePlan.Id,
+            AppSettings =
+            {
+                // {"WEBSITE_RUN_FROM_PACKAGE", codeBlobUrl},
+                {"APPINSIGHTS_INSTRUMENTATIONKEY", appInsights.InstrumentationKey},
+                // {"APPLICATIONINSIGHTS_CONNECTION_STRING", appInsights.InstrumentationKey.Apply(key => $"InstrumentationKey={key}")},
+                {"ApplicationInsightsAgent_EXTENSION_VERSION", "~2"},
+            },
+            HttpsOnly = true,
+            Identity = new AppServiceIdentityArgs
+            {
+                Type = "SystemAssigned"
+            },
+        });
+        //ref https://www.pulumi.com/docs/reference/pkg/azure/keyvault/keyvault/
+        var current = Output.Create(Pulumi.Azure.Core.GetClientConfig.InvokeAsync());
+        var exampleKeyVault = new KeyVault("exampleKeyVault", new Pulumi.Azure.KeyVault.KeyVaultArgs
+        {
+            Location = resourceGroup.Location,
+            ResourceGroupName = resourceGroup.Name,
             EnabledForDiskEncryption = true,
             TenantId = current.Apply(current => current.TenantId),
             SoftDeleteEnabled = true,
@@ -41,14 +69,13 @@ class MyStack : Stack
             SkuName = "standard",
             AccessPolicies = 
             {
-                new Azure.KeyVault.Inputs.KeyVaultAccessPolicyArgs
+                new KeyVaultAccessPolicyArgs
                 {
                     TenantId = current.Apply(current => current.TenantId),
                     ObjectId = current.Apply(current => current.ObjectId),
                     KeyPermissions = 
                     {
                         "get",
-                        "ManageContacts",
                     },
                     SecretPermissions = 
                     {
@@ -60,19 +87,10 @@ class MyStack : Stack
                     },
                 },
             },
-            NetworkAcls = new Azure.KeyVault.Inputs.KeyVaultNetworkAclsArgs
+            NetworkAcls = new KeyVaultNetworkAclsArgs
             {
                 DefaultAction = "Deny",
                 Bypass = "AzureServices",
-            },
-            Contacts = 
-            {
-                new Azure.KeyVault.Inputs.KeyVaultContactArgs
-                {
-                    Email = "example@example.com",
-                    Name = "example",
-                    Phone = "0123456789",
-                },
             },
             Tags = 
             {
@@ -81,8 +99,13 @@ class MyStack : Stack
         });    
         // Export the connection string for the storage account
         this.ConnectionString = storageAccount.PrimaryConnectionString;
+        this.appID = webapp.Id;
+        this.Outbound = webapp.OutboundIpAddresses;
     }
 
     [Output]
     public Output<string> ConnectionString { get; set; }
+    public Output<string> appID { get; set; }
+    public Output<string> Outbound { get; set; }
+
 }
